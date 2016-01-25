d602fea8402e04e9b91543448ca7d7b5decd983e
OKAZAKI Yasuo <yasuo_okazaki@fandmnet.com>
2016-01-25 15:17:19 +0900

    Initial Commit

---
+++ PlotDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PlotDemo
+//
+//  Created by OKAZAKI Yasuo on 2016/01/25.
+//  Copyright © 2016年 F&Mnet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
